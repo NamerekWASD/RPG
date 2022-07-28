ece3f374ae23094e2d028fbf49f206b1372bd6f0
NamerekWASD <1488nikolay1488@gmail.com>
2022-07-28 16:58:51 +0300

    Separated keys in class changed on object map.

--- core/src/main/java/com/company/rpgame/configuration/preferences/ControlsData.java
+++ core/src/main/java/com/company/rpgame/configuration/preferences/ControlsData.java
@@ -1,18 +1,10 @@
 package com.company.rpgame.configuration.preferences;
 
+import com.badlogic.gdx.utils.ObjectMap;
 import com.company.rpgame.service.controls.ControlType;
 
 public class ControlsData {
-    /** Up movement shortcut. */
-    public int up;
-    /** Down movement shortcut. */
-    public int down;
-    /** Left movement shortcut. */
-    public int left;
-    /** Right movement shortcut. */
-    public int right;
-    /** Jump shortcut. */
-    public int jump;
+    public ObjectMap<String, Integer> keys;
     /** Type of controls */
     public ControlType type;
     /** Additional data. Might be used for device ID. */
