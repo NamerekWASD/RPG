441264f8e671fed53e9c43a51d4fe67deb01ec68
Nykolai <1488nikolay1488@gmail.com>
2022-12-20 20:24:47 +0200

    Updated BodyBuilder and Box2DService classes, added camera service

--- core/src/main/java/com/company/rpgame/helpers/Box2D/BodyBuilder.java
+++ core/src/main/java/com/company/rpgame/helpers/Box2D/BodyBuilder.java
@@ -1,61 +1,58 @@
 package com.company.rpgame.helpers.Box2D;
 
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.*;
+import com.company.rpgame.helpers.Box2D.components.Size;
 import com.company.rpgame.helpers.Constants;
 
 import static com.company.rpgame.helpers.Constants.PPM;
 
-public class BodyBuilder {
-    public static Body createBox(World world, float x, float y, int width, int height, boolean isStatic, boolean fixedRotation) {
-        Body pBody;
-        BodyDef def = new BodyDef();
-
-        if(isStatic)
-            def.type = BodyDef.BodyType.StaticBody;
-        else
-            def.type = BodyDef.BodyType.DynamicBody;
-
-        def.position.set(x / PPM, y / PPM);
-        def.fixedRotation = fixedRotation;
-        pBody = world.createBody(def);
-        pBody.setUserData("wall");
-
-        PolygonShape shape = new PolygonShape();
-        shape.setAsBox(width / 2f / PPM, height / 2f / PPM);
-
-        FixtureDef fd = new FixtureDef();
-        fd.shape = shape;
-        fd.density = 1.0f;
-        fd.filter.categoryBits = Constants.BIT_WALL;
-        fd.filter.maskBits = Constants.BIT_PLAYER | Constants.BIT_WALL | Constants.BIT_SENSOR;
-        fd.filter.groupIndex = 0;
-        pBody.createFixture(fd);
-        shape.dispose();
-        return pBody;
+public final class BodyBuilder {
+    public static Body createBody(World world, Vector2 position, Size size,
+                                  boolean isStatic, boolean isFixedRotation) {
+        return createBody(world, position, size, isStatic, isFixedRotation,
+                Constants.BIT_WALL, (short) (Constants.BIT_PLAYER | Constants.BIT_WALL| Constants.BIT_SENSOR),
+                (short) 0);
     }
 
-    public static Body createBox(final World world, float x, float y, float w, float h,
-                                 boolean isStatic, boolean canRotate, short cBits, short mBits, short gIndex) {
-        BodyDef bodyDef = new BodyDef();
-        bodyDef.fixedRotation = canRotate;
-        bodyDef.position.set(x / PPM, y / PPM);
+    public static Body createBody(final World world, Vector2 position, Size size,
+                                  boolean isStatic, boolean isFixedRotation, short cBits, short mBits, short gIndex) {
 
-        if(isStatic) {
-            bodyDef.type = BodyDef.BodyType.StaticBody;
-        } else {
-            bodyDef.type = BodyDef.BodyType.DynamicBody;
-        }
+        Body body = defineBody(world, position, isStatic, isFixedRotation);
+        initializeBodyWithFixture(body, size, cBits, mBits, gIndex);
+        return body;
+    }
 
+    private static Body defineBody(World world, Vector2 position, boolean isStatic, boolean isFixedRotation){
+        BodyDef bodyDefinition = new BodyDef();
+        bodyDefinition.position.set(position.x / PPM, position.y / PPM);
+        bodyDefinition.fixedRotation = isFixedRotation;
+        defineBodyType(bodyDefinition, isStatic);
+        return world.createBody(bodyDefinition);
+    }
+
+    private static void defineBodyType(BodyDef bodyDefinition, boolean isStatic){
+        bodyDefinition.type = isStatic ? BodyDef.BodyType.StaticBody : BodyDef.BodyType.DynamicBody;
+    }
+
+    private static void initializeBodyWithFixture(Body body, Size size, short cBits, short mBits, short gIndex){
+        FixtureDef fixtureDef = createFixture(size, cBits, mBits, gIndex);
+        body.createFixture(fixtureDef);
+    }
+    private static FixtureDef createFixture(Size size, short cBits, short mBits, short gIndex){
         PolygonShape shape = new PolygonShape();
-        shape.setAsBox(w / PPM / 2, h / PPM / 2);
-
+        shape.setAsBox(size.getWidth() / PPM / 2, size.getHeight() / PPM / 2);
         FixtureDef fixtureDef = new FixtureDef();
         fixtureDef.shape = shape;
-        fixtureDef.density = 1.0f;
+        fixtureDef.density = 1f;
+        fixtureDef.friction = 0f;
+        assignFilters(fixtureDef, cBits, mBits, gIndex);
+        return fixtureDef;
+    }
+
+    private static void assignFilters(FixtureDef fixtureDef, short cBits, short mBits, short gIndex){
         fixtureDef.filter.categoryBits = cBits; // Is a
         fixtureDef.filter.maskBits = mBits; // Collides with
         fixtureDef.filter.groupIndex = gIndex;
-
-        return world.createBody(bodyDef).createFixture(fixtureDef).getBody();
     }
 }
--- core/src/main/java/com/company/rpgame/service/Box2DService.java
+++ core/src/main/java/com/company/rpgame/service/Box2DService.java
@@ -1,60 +1,44 @@
 package com.company.rpgame.service;
 
-import static com.company.rpgame.helpers.Constants.DEFAULT_STEP;
-import static com.company.rpgame.helpers.Constants.PPM;
-import static com.company.rpgame.helpers.Constants.EARTH_GRAVITY;
-
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
-import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
-import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.World;
-import com.badlogic.gdx.utils.Array;
-import com.badlogic.gdx.utils.ObjectMap;
-import com.badlogic.gdx.utils.viewport.FitViewport;
-import com.badlogic.gdx.utils.viewport.Viewport;
-import com.company.rpgame.Application;
-import com.company.rpgame.entities.Player;
-import com.company.rpgame.entities.base.ParticleEmitterTest;
+import com.company.rpgame.exception.NoSpawnPointException;
+import com.company.rpgame.helpers.Box2D.TiledMapUtils;
 import com.company.rpgame.service.listeners.WorldListener;
+import com.company.rpgame.service.ui.CameraService;
 import com.github.czyzby.autumn.annotation.Component;
 import com.github.czyzby.autumn.annotation.Inject;
-
 import net.dermetfan.gdx.physics.box2d.Box2DMapObjectParser;
+
+import java.util.Objects;
+
+import static com.company.rpgame.helpers.Constants.*;
 
 
 @Component
 public class Box2DService extends Game implements Screen {
+    private final Box2DMapObjectParser parser = new Box2DMapObjectParser(1/PPM);
+    @Inject
+    private CameraService cameraService;
     private Box2DDebugRenderer bdr;
 
-    private Player player;
     private World world;
     private TiledMap map;
-
-    private final Viewport viewport = new FitViewport(Application.WIDTH, Application.HEIGHT);
-
     private OrthogonalTiledMapRenderer tmr;
-
-    private SpriteBatch batch;
-
-    @Inject private ControlsService controlsService;
 
     private Status status;
 
-    private ParticleEmitterTest particleEmitter;
 
-    public Player getPlayer() {
-        return player;
-    }
 
     enum Status{
         RUN,
@@ -65,31 +49,16 @@ public class Box2DService extends Game implements Screen {
     public void create() {
         dispose();
 
-        particleEmitter = new ParticleEmitterTest();
-
-        particleEmitter.create();
-
         bdr = new Box2DDebugRenderer();
-
         status = Status.RUN;
-
-        batch = new SpriteBatch();
-
-        world = new World(EARTH_GRAVITY, true);
-
-        player = new Player(controlsService.getControl(), world, viewport);
+        world = new World(GRAVITY, true);
 
         world.setContactListener(new WorldListener());
 
-        map = new TmxMapLoader().load("maps/test/testmap.tmx");
-        Box2DMapObjectParser parser = new Box2DMapObjectParser(1f/PPM);
+        map = new TmxMapLoader().load("maps/test/newMap.tmx");
         parser.load(world, map);
-        for (ObjectMap.Entry<String, Fixture> fixture:
-             parser.getFixtures()) {
-            fixture.value.setUserData(this);
-        }
 
-        tmr = new OrthogonalTiledMapRenderer(map, parser.getUnitScale()*PPM);
+        tmr = new OrthogonalTiledMapRenderer(map);
     }
 
     @Override
@@ -100,21 +69,20 @@ public class Box2DService extends Game implements Screen {
     @Override
     public void render(float delta) {
         if(status == Status.RUN){
-            Gdx.gl.glClearColor(.25f,.5f,.8f,.8f);
+            Gdx.gl.glClearColor(.25f,.5f,.8f,.9f);
             Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-            particleEmitter.render(batch, delta);
             update(delta);
         }else{
+            Gdx.gl.glClearColor(1f, 1f, 1f, 1f);
             Gdx.gl.glClear(GL20.GL_DEPTH_BUFFER_BIT);
         }
-        player.render(batch, delta);
-        bdr.render(world, viewport.getCamera().combined.scl(PPM));
         tmr.render();
-        viewport.apply();
+        bdr.render(world, cameraService.getProjectionMatrix().scl(PPM));
     }
 
+    @Override
     public void resize(int width, int height) {
-        viewport.update(width, height);
+        cameraService.resize(width, height);
     }
 
     @Override
@@ -150,45 +118,28 @@ public class Box2DService extends Game implements Screen {
             map.dispose();
             map = null;
         }
-        if(player !=null){
-            player.dispose();
-            player = null;
-        }
-
-        if(particleEmitter != null){
-            particleEmitter.dispose();
-            particleEmitter = null;
-        }
     }
 
-    public void update(final float delta) {
+    private void update(final float ignoredDelta) {
         world.step(DEFAULT_STEP, 8, 3);
-        player.update(delta);
-        tmr.setView((OrthographicCamera) viewport.getCamera());
-        cameraUpdate(delta);
-        batch.setProjectionMatrix(viewport.getCamera().combined);
+        tmr.setView((OrthographicCamera) cameraService.getCamera());
     }
-
-    private void cameraUpdate(final float delta){
-        Vector3 position = viewport.getCamera().position;
-        position.x = player.getBodyPosition().x * PPM;
-        position.y = player.getBodyPosition().y * PPM;
-
-        viewport.getCamera().position.set(position);
-
-        viewport.getCamera().update();
-    }
-
-    public void initiateControls(final InputMultiplexer inputMultiplexer) {
-        player.getControl().attachInputListener(inputMultiplexer);
-        particleEmitter.attachInputListener(inputMultiplexer);
-    }
-
-    public int getPlayerHealthPoints (){
-        return player.getHealthPoints();
-    }
-
     public boolean isRunning() {
         return status == Status.RUN;
+    }
+    public World getWorld() {
+        return world;
+    }
+    public Vector2 getWorldSpawnPoint() throws NoSpawnPointException {
+        Vector2 spawnPoint = new Vector2();
+
+        RectangleMapObject spawnPointRectangle = TiledMapUtils.fetchRectangle(map, "Utils", "spawnPoint");
+        if(Objects.isNull(spawnPointRectangle)){
+            throw new NoSpawnPointException("Map must have spawn point!");
+        }
+        spawnPoint.x = spawnPointRectangle.getRectangle().x;
+        spawnPoint.y = spawnPointRectangle.getRectangle().y;
+
+        return spawnPoint;
     }
 }
